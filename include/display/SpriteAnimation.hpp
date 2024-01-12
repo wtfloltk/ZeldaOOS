d5f4d399061e5e2963008592c0c6af1d2d8d0204
wtfloltk <153698146+wtfloltk@users.noreply.github.com>
2024-01-12 03:02:11 -0500

    Update SpriteAnimation.hpp

--- include/display/SpriteAnimation.hpp
+++ include/display/SpriteAnimation.hpp
@@ -31,7 +31,8 @@ struct SpriteAnimation {
 	bool isPlaying;
 	s16 (*position)[2];
 	char* ToString() {
-		return ((char*)("u16 _size, u8 *_tabAnim, u16 _delay, s16 _position[][2]\n" + (char*)this->size + " " + (char*)this->tabAnim + " " + (char*)this->delay + " " + (char*)this->timer + " " + (char*)this->isPlaying + " " + (char*)this->position[0] + "," + (char*)this->position[1]));
+		return ((char*)("u16 _size, u8 *_tabAnim, u16 _delay, s16 _position[][2]\n" + (char*)(this->size) + " " + (char*)(this->tabAnim) + " " + (char*)(this->delay)
+			+ " " + (char*)(this->timer) + " " + (char*)(this->isPlaying) + " " + (char*)(this->position[0]) + "," + (char*)(this->position[1])));
 	}	
 };
 
